c31f51477a93f61861fde8dae56bd537549d82a2
Usama Imdad <usamaimdadsian@gmail.com>
2022-12-20 16:55:18 +0500

    init

---
+++ README.md
@@ -0,0 +1,29 @@
+<h1 align="center">Welcome to E-Commerce Product Unit Sale Classification 👋</h1>
+<p>
+  <a href="https://twitter.com/UsamaImdadSian" target="_blank">
+    <img alt="Twitter: UsamaImdadSian" src="https://img.shields.io/twitter/follow/UsamaImdadSian.svg?style=social" />
+  </a>
+</p>
+
+> In this project, a kaggle dataset of wish.com is used to classify the unit sale range of the products. 7 different model are used to test which works best on the dataset. Notebook in this repository also contains the code for deployment on Azure ML workspace.
+
+### ✨ [Demo](https://www.youtube.com/watch?v=RwSzRrWs5Tk)
+
+## Usage
+
+```sh
+This is the python code which can be run in jupyter notebook.
+```
+
+## Author
+
+👤 **Usama Imdad**
+
+* Website: https://usamaimdad.cf
+* Twitter: [@UsamaImdadSian](https://twitter.com/UsamaImdadSian)
+* Github: [@usamaimdadsian](https://github.com/usamaimdadsian)
+* LinkedIn: [Usama Imdad](https://www.linkedin.com/in/usama-imdad/)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
---
+++ score.py
@@ -0,0 +1,43 @@
+import os
+import logging
+import json
+import numpy
+import joblib
+import pandas as pd
+
+
+
+def init():
+    """
+    This function is called when the container is initialized/started, typically after create/update of the deployment.
+    You can write the logic here to perform init operations like caching the model in memory
+    """
+    global model
+
+
+    # AZUREML_MODEL_DIR is an environment variable created during deployment.
+    # It is the path to the model folder (./azureml-models/$MODEL_NAME/$VERSION)
+    # Please provide your model's folder name if there is one
+    model_path = 'azureml-models/summer_product_sale_model/1/model/model.pkl'
+
+    # deserialize the model file back into a sklearn model
+    model = joblib.load(model_path)
+    logging.info("Init complete")
+
+
+def run(raw_data):
+    """
+    This function is called for every invocation of the endpoint to perform the actual scoring/prediction.
+    In the example we extract the data from the json input and call the scikit-learn model's predict()
+    method and return the result back
+    """
+    logging.info("model 1: request received")
+    data = json.loads(raw_data)
+    df = pd.DataFrame(pd.Series(data)).T
+
+
+    # data = json.loads(raw_data)["data"]
+    # data = numpy.array(data)
+    result = model.predict(df)
+    logging.info("Request processed")
+    return result.tolist()
